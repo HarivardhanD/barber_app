ae7d7e3ab2f0f06a68be4503a595f315bece6a8b
HarivardhanD <harivardhanmallya04@gmail.com>
2025-08-11 22:58:37 +0530

    bookinng_admin functionality done

--- lib/Admin/booking_admin.dart
+++ lib/Admin/booking_admin.dart
@@ -1,3 +1,5 @@
+import 'package:barber_app/services/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BookingAdmin extends StatefulWidget {
@@ -8,104 +10,112 @@ class BookingAdmin extends StatefulWidget {
 }
 
 class _BookingAdminState extends State<BookingAdmin> {
+  Stream? BookingStream;
+
+  getontheload() async {
+    BookingStream = await DatabaseMethods().getBookings();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getontheload();
+  }
+
+  Widget allBookings() {
+    return StreamBuilder(
+      stream: BookingStream,
+      builder: (context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        return ListView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          itemCount: snapshot.data.docs.length,
+          itemBuilder: (context, index) {
+            DocumentSnapshot ds = snapshot.data.docs[index];
+            return Material(
+              elevation: 10,
+              borderRadius: BorderRadius.circular(30),
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Name : ${ds["UserName"]}",
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Service : ${ds["Service"]}",
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Date : ${ds["Date"]}",
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Time : ${ds["Time"]}",
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width / 2,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                      ),
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        "Done",
+                        style: TextStyle(
+                            fontSize: 29,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold (body: Container(
-      
-      
-      child: Column(
-        
-        
-        
-        children: [
-          
-           
-          Center(child: Container(
-            padding: EdgeInsets.only(left: 10,top: 20),
-            
-           
-            
-            
-            
-            child: Text("All Bookings ",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold,color: const Color.fromARGB(255, 15, 14, 14)),),
-          ),),
-          SizedBox(height: 30,),
-
-          Material(
-            elevation: 10,
-            borderRadius: BorderRadius.circular(30),
-            
-
-            child: Container(
-              
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height/3,
-              padding: EdgeInsets.only(left: 10,top: 10),
-              decoration: BoxDecoration(color: Colors.red,borderRadius: BorderRadius.circular(30),),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                      "Name : Amma",
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Service : Haircut",
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Date : 4/11/2003",
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Time : 12:00 pm",
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                    SizedBox(height: 30,),
-                    Container(
-                      padding: EdgeInsets.only(left: 60),
-                      width: MediaQuery.of(context).size.width/2,
-                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white,),
-                      child: Text("Done",style: TextStyle(fontSize: 29,fontWeight: FontWeight.bold,color: Colors.black),),
-                    )
-
-
-
-                    
-                   
-                  ],
-             
-                
-              ),
-              
-
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            Text(
+              "All Bookings",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 15, 14, 14)),
             ),
-          
-            
-
-          ),
-           
-                    
-        ],
-        
-        
+            SizedBox(height: 30),
+            Expanded(child: allBookings()),
+          ],
+        ),
       ),
-      
-    ),);
+    );
   }
 }
